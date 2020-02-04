4650fb364d58a3c19aa781948781ea2885ad6204
Suresh <reddysureshcmc@gmail.com>
2020-02-04 21:36:13 +0530

    added file open feature

--- src/pages/Chakra.tsx
+++ src/pages/Chakra.tsx
@@ -1,54 +1,55 @@
 
 import React from 'react';
 
-import { Plugins,  FilesystemDirectory, Capacitor, FilesystemEncoding } from '@capacitor/core';
- 
-import { IonContent, IonText, IonButton    } from '@ionic/react';
+import { Plugins, FilesystemDirectory, Capacitor, FilesystemEncoding } from '@capacitor/core';
+import { FileOpener } from '@ionic-native/file-opener/ngx';
+import { IonContent, IonText, IonButton } from '@ionic/react';
 import './Chakra.css';
 export const Chakra: React.FC = () => {
 
+  let fileOpener: FileOpener;
+
   let generatePDF = () => {
-      console.log('Generate pdf called');
-      const { Filesystem } = Plugins;
+    console.log('Generate pdf called');
+    const { Filesystem } = Plugins;
 
     if (Capacitor.isNative) {
-        // Save the PDF to the device
-        const fileName = 'timesheet.txt';
-        try {
-          Filesystem.writeFile({
-            path: fileName,
-            data: "This is sample text",
+      // Save the PDF to the device
+      const fileName = 'timesheet.txt';
+      try {
+        Filesystem.writeFile({
+          path: fileName,
+          data: "This is sample text",
+          directory: FilesystemDirectory.Documents,
+          encoding: FilesystemEncoding.UTF8
+        }).then((writeFileResult) => {
+          Filesystem.getUri({
             directory: FilesystemDirectory.Documents,
-            encoding: FilesystemEncoding.UTF8
-          }).then((writeFileResult) => {
-            Filesystem.getUri({
-                directory: FilesystemDirectory.Documents,
-                path: fileName
-            }).then((getUriResult) => {
-                const path = getUriResult.uri;
-                // this.fileOpener.open(path, 'application/pdf')
-                // .then(() => console.log('File is opened'))
-                // .catch(error => console.log('Error openening file', error));
-            }, (error) => {
-                console.log(error);
-            });
+            path: fileName
+          }).then((getUriResult) => {
+            const path = getUriResult.uri;
+            fileOpener.open(path, 'text/plain').then(() => console.log('File is opened'))
+              .catch(error => console.log('Error openening file', error));
+          }, (error) => {
+            console.log(error);
           });
-        } catch (error) {
-          console.error('Unable to write file', error);
-        }
-      } else {
-        console.log('Running in web');
-        // On a browser simply use download
-        // this.pdfObj.download();
+        });
+      } catch (error) {
+        console.error('Unable to write file', error);
+      }
+    } else {
+      console.log('Running in web');
+      // On a browser simply use download
+      // this.pdfObj.download();
     }
   }
- 
+
   console.log('Chakra called');
-  return ( 
-     
-      <IonContent>
-         <table className="chakra-table">
-          <tbody>
+  return (
+
+    <IonContent>
+      <table className="chakra-table">
+        <tbody>
           <tr>
             <td>first</td>
             <td></td>
@@ -73,16 +74,16 @@ export const Chakra: React.FC = () => {
             <td></td>
             <td></td>
           </tr>
-          </tbody>
-        </table>
+        </tbody>
+      </table>
 
-        <div>
-          <IonButton onClick={generatePDF}>Export PDF</IonButton>
+      <div>
+        <IonButton onClick={generatePDF}>Export PDF</IonButton>
 
-        </div>
-      </IonContent>
-     
+      </div>
+    </IonContent>
+
   );
 };
 
- 
+
